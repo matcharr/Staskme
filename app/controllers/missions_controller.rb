4234ca945ecf123a0b204cd876bcebf6073cedd1
rosvali <rosval6578@gmail.com>
2020-09-10 18:37:20 +0200

    bug fixed

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,4 +1,6 @@
 class MissionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
    def new
       @mission = Mission.new
    end
@@ -14,13 +16,28 @@ class MissionsController < ApplicationController
    def create
       @mission = Mission.new(mission_params)
       @mission.user_id = current_user.id
+      #valeur par default pour le MVP
+      @mission.category = Category.last
       if @mission.save
-        redirect_to root_path
-      else
-        render 'new'
-      end
-   end
+        @amount = 1000
+        customer = Stripe::Customer.create({
+          email: params[:stripeEmail],
+          source: params[:stripeToken],
+        })
 
+        charge = Stripe::Charge.create({
+          customer: customer.id,
+          amount: @amount,
+          description: 'Rails Stripe customer',
+          currency: 'chf',
+        })
+        redirect_to root_path
+      end
+      puts @mission.errors.full_messages
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_mission_path
+   end
 
    private
 
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,10 +3,10 @@ class Mission < ApplicationRecord
 	belongs_to :user
 
 	validates :title, presence: true
-	validates :title, length: { in: 3..100 }
+	# validates :title, length: { in: 3..100 }
 
 	validates :description, presence: true
-	validates :description, length: { in: 20..200 }
+	# validates :description, length: { in: 20..200 }
 
 	validates :start_date, presence: true
 	validate :in_the_past?
