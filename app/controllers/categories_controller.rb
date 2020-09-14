5fd28fdf31287a63f85c59dba4f84d75171cf9ca
rosvali <rosval6578@gmail.com>
2020-09-14 15:08:16 +0200

    adding categories card

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
-
-    def index
-        @categories = Category.all
-    end
+  def index
+		@categories = Category.all
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ Category.destroy_all
    Mission.create(title:Faker::Company.industry, description:Faker::Movie.quote, start_date:"2021-01-01")
 end
 
-Category.create(title: "Courses, livraison", image: "https://images.pexels.com/photos/1005638/pexels-photo-1005638.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+Category.create(title: "Courses, livraison", image: "https://images.unsplash.com/photo-1588964895597-cfccd6e2dbf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80")
 Category.create(title: "Réception marchandises ou personnes", image: "https://images.unsplash.com/photo-1516733968668-dbdce39c4651?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
 Category.create(title: "Ménage", image: "https://images.unsplash.com/photo-1563453392212-326f5e854473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
 Category.create(title: "Préparation de dîner", image: "https://images.unsplash.com/photo-1556911220-bff31c812dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
-Category.create(title: "Garde d'enfants urgente", image: "https://images.unsplash.com/photo-1533499966477-9333968a4e28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60")
+Category.create(title: "Garde d'enfants urgente", image: "https://images.unsplash.com/photo-1557939574-a2cb399f443f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
