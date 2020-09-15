2564e40f253c501f5cdeeed6f4344b1c355867b7
mocadi-dot <mdg@dataddict.info>
2020-09-16 01:58:35 +0200

    enroll user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :enrolments
+  has_many :missions, through: :enrolments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
