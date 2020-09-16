1e7cf21228dc6a05993e5ba7ad587db9f7c7a87f
mocadi-dot <mdg@dataddict.info>
2020-09-16 21:57:37 +0200

    routes contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'categories#index'
+  get '/contact', to: 'contact#index'
   devise_for :users
   resources :users, only: [:show, :update]
   resources :missions, except: [:edit, :update]
+  
   namespace :admin do
       root 'admin#index'
       resources :dashboard, only:[:show]
