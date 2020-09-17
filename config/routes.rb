d6bed00996b86bdf63fa5ec06f367b0dbaa7cc55
mocadi-dot <mdg@dataddict.info>
2020-09-17 16:36:29 +0200

    màj routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'contact#index'
   devise_for :users
   resources :users, only: [:show, :update]
-<<<<<<< HEAD
-  resources :missions, except: [:edit, :update]
-  
-=======
   resources :missions, except: [:edit]
   resources :enrollments, only: [:create, :show]
->>>>>>> 7c0f168ccc21be23b57a1ba0f83844ffee19b7b9
   namespace :admin do
       root 'admin#index'
-      resources :dashboard, only:[:show]
- end
+    end
 end
