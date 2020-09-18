fea881ee96dda5ff345bffb1ee37c6edd3f15cbc
rosvali <rosval6578@gmail.com>
2020-09-18 10:21:01 +0200

    heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   after_create :welcome_send
 
   def welcome_send
-    UserMailer.welcome_email(self).deliver_now!
+    UserMailer.welcome_email(self).deliver_now
   end
 
 	has_many :enrollments
--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => ENV['MAIL_API'],
-#   :password => ENV['MAIL_SECRET'],
-#   :domain => 'catweb.fr',
-#   :address => 'in-v3.mailjet.com',
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-# }
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAIL_API'],
+  :password => ENV['MAIL_SECRET'],
+  :domain => 'catweb.fr',
+  :address => 'in-v3.mailjet.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
